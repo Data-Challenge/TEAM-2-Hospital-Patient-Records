eb3c14ec82b499b12f0a620f3a40fd80b2cf6284
JongbeomShim <rz22lf@brocku.ca>
2026-02-27 16:03:50 -0500

    Update main.py
    Update 1.4 and 1.5 for analysis

--- main.py
+++ main.py
@@ -50,24 +50,37 @@ print() #linebreak
 # 0-19
 underage = cleaned_df[cleaned_df['age'].between(0, 19)]
 underage_emergency_rate = round((underage['admission_type'] == 'Emergency').mean() * 100, 2)
-print(f"Underage emergency admission rate: {underage_emergency_rate}%")
 
 # 20-59
 adult = cleaned_df[cleaned_df['age'].between(20, 59)]
 adult_emergency_rate = round((adult['admission_type'] == 'Emergency').mean() * 100, 2)
-print(f"Adult emergency admission rate: {adult_emergency_rate}%")
 
 # 60+
 senior = cleaned_df[cleaned_df['age'].between(20, 59)]
 senior_emergency_rate = round((senior['admission_type'] == 'Emergency').mean() * 100, 2)
-print(f"Senior emergency admission rate: {senior_emergency_rate}%")
 
-
-print() #linebreak 
+print("\n== 1.4 - What percentage of patients in each age group require emergency admission? ==")
+print(f"Underage emergency admission rate: {underage_emergency_rate}%")
+print(f"Adult emergency admission rate: {adult_emergency_rate}%")
+print(f"Senior emergency admission rate: {senior_emergency_rate}%\n")
 
 # 1.5 - Is there a relationship between length of stay and total charges?
+charge_per_day = []
+for x in cleaned_df.index:
+    charge_per_day.append(round(float(cleaned_df.loc[x, 'total_charges'] / cleaned_df.loc[x, 'length_of_stay']), 2))
+charge_per_day = pd.DataFrame({'charge_per_day' : charge_per_day})
+charge_per_day['total_charges'] = cleaned_df['total_charges']
 
-print() #linebreak 
+largest_charge_per_day = charge_per_day['charge_per_day'].max()
+lowest_charge_per_day = charge_per_day['charge_per_day'].min()
+
+diff_charge_per_day = largest_charge_per_day - lowest_charge_per_day
+
+print("\n== 1.5 - Is there a relationship between length of stay and total charges? ==")
+print(f"Largest Charge per Day: {largest_charge_per_day}")
+print(f"Lowest Charge per Day: {lowest_charge_per_day}")
+print(f"Difference of Largest and Lowest Charge per Day: {largest_charge_per_day}")
+print("\nThere are almost no relationship between length of stay and total charges from this data\nsince there is large difference between largest charge per day and lowest charge per day.\n")
 
 # 1.6 - Which department treats the oldest patients on average?
 
