c75c48e74bf798002f2fdd60afc4bf8401ed62b2
js22ed <js22ed@brocku.ca>
2026-02-27 10:42:47 -0500

    Update main.py
    update to print statement in exercise 6

--- main.py
+++ main.py
@@ -61,6 +61,7 @@ print(f"Senior emergency admission rate: {senior_emergency_rate}%")
 # 1.6 - Which department treats the oldest patients on average?
 
 oldpatient_treatment = cleaned_df.groupby("department")["age"].mean().sort_values(ascending=false)
+print(oldpatient_treatment) 
 
 #print(cleaned_df)
 print(cleaned_df.info())
