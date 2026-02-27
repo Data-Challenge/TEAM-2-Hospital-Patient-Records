deddbdf329281ffa8cfd595da97a885a53f49ee6
JongbeomShim <rz22lf@brocku.ca>
2026-02-27 08:41:55 -0500

    Update main.py
    Add Clean up data code

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ import pandas as pd
 
 pd.options.display.max_rows = 999
 
-df = pd.read_csv("hospital.csv")
+original_df = pd.read_csv("hospital.csv")
+cleaned_df = pd.read_csv("hospital.csv")
 
-print(df)
+# Clean up insurance data
+cleaned_df.fillna({"insurance" : "N\A"}, inplace = True)
+
+# Clean up age data
+for x in cleaned_df.index:
+    if cleaned_df.loc[x, "age"] < 0:
+        cleaned_df.drop(x, inplace = True)
+
+# Clean up gender data
+for x in cleaned_df.index:
+    if cleaned_df.loc[x, "gender"] == "M":
+        cleaned_df.loc[x, "gender"] = "Male"
+    if cleaned_df.loc[x, "gender"] == "F":
+        cleaned_df.loc[x, "gender"] = "Female"
+
+print(cleaned_df)
+print(cleaned_df.info())
