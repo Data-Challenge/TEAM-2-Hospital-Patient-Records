5f6b884d64e26381124cd6e149b099aa889277b1
shim <shim@core>
2026-02-27 10:22:25 -0500

    Update main.py; 1.4 - What percentage of patients in each age group require emergency admission?

--- main.py
+++ main.py
@@ -21,19 +21,38 @@ for x in cleaned_df.index:
     if cleaned_df.loc[x, "gender"] == "F":
         cleaned_df.loc[x, "gender"] = "Female"
 
-print(cleaned_df)
-print(cleaned_df.info())
+# ANALYSIS
 
-
-#ANALYSIS
-
-#1.1 - what is the average stay by admission type? 
+# 1.1 - what is the average stay by admission type? 
 cleaned_df.groupby("admission_type")["length_of_stay"].mean()
 
-#1.2 - Which department has the highest readmission rate? 
+# 1.2 - Which department has the highest readmission rate? 
 readmission_rate = cleaned_df.groupby("department")["readmitted"].mean()
 highest_department = readmission_rate.idxmax()
 highest_value = readmission_rate.max()
 
-#1.3 - How does insurance type affect total charges (list charges by type - is there any causality?) 
+# 1.3 - How does insurance type affect total charges (list charges by type - is there any causality?) 
 cleaned_df.groupby("insurance")["total_charges"].mean()
+
+# 1.4 - What percentage of patients in each age group require emergency admission?
+# 0-19
+underage = cleaned_df[cleaned_df['age'].between(0, 19)]
+underage_emergency_rate = round((underage['admission_type'] == 'Emergency').mean() * 100, 2)
+print(f"Underage emergency admission rate: {underage_emergency_rate}%")
+
+# 20-59
+adult = cleaned_df[cleaned_df['age'].between(20, 59)]
+adult_emergency_rate = round((adult['admission_type'] == 'Emergency').mean() * 100, 2)
+print(f"Adult emergency admission rate: {adult_emergency_rate}%")
+
+# 60+
+senior = cleaned_df[cleaned_df['age'].between(20, 59)]
+senior_emergency_rate = round((senior['admission_type'] == 'Emergency').mean() * 100, 2)
+print(f"Senior emergency admission rate: {senior_emergency_rate}%")
+
+# 1.5 - Is there a relationship between length of stay and total charges?
+
+# 1.6 - Which department treats the oldest patients on average?
+
+#print(cleaned_df)
+print(cleaned_df.info())
